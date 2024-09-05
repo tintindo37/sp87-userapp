06d1f6c3bebe5cfc819bd751bc84707462645f25
tintindo37 <tintin.do.37@gmail.com>
2024-09-05 02:31:49 +0200

    prod ready for merge

--- js/updatetxt.js
+++ js/updatetxt.js
@@ -2,8 +2,15 @@ function showFeatureBox(text) {
     const featureBox = document.getElementById('featureBox');
     featureBox.innerText = text;
     featureBox.style.display = 'block';
+    const butt = document.createElement("button");
+    butt.innerHTML = "Zamknij update";
+    butt.setAttribute("onclick","patch()")
+    featureBox.appendChild(butt);
 }
-
+function patch(){
+    const featureBox = document.getElementById('featureBox');
+    featureBox.style.display = 'none';
+}
 async function fetchFeatureText() {
     const response = await fetch('https://raw.githubusercontent.com/tintindo37/sp87-userapp/testing-main/patchnote/latest.txt.txt'); // Update this URL
     const text = await response.text();
